98422e15c08dbf0b8f07f40d0e5412cea2c28889
philrussel21 <phil.antiporda21@gmail.com>
2020-09-08 15:44:14 +0930

    Updates with solutions to challenge

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const phones = require(`./phones`)
+const { phones } = require(`./phones`)
+const last2YearCheck = (arr) => {
+    return new Promise((resolve, reject) => {
+        const validYears = arr.filter(({ year }) => (year >= 2018 && year <= 2020));
+        validYears.length !== 0 ? resolve(validYears) : reject('Nothing found from the last two years!')
+    })
+}
 
-// const last2YearCheck
-// const notAppleOrGoogle
-// const afford
+const notAppleOrGoogle = (arr) => {
+    return new Promise((resolve, reject) => {
+        const validManufacturers = arr.filter(({ manufacturer }) => (manufacturer !== 'Google' && manufacturer !== 'Apple'))
+        validManufacturers.length !== 0 ? resolve(validManufacturers) : reject('No phones from other manufacturers found')
+    })
+}
+
+const afford = (arr) => {
+    return new Promise((resolve, reject) => {
+        const canAfford = arr.filter(({ cost }) => cost <= 500)
+        canAfford.length !== 0 ? resolve(canAfford) : reject('Not enough money to purchase something today')
+    })
+}
+const options = (arr) => {
+    return console.log(arr.map(({ name }) => { return { "name": name } }))
+}
 
 
-// const options = 
+last2YearCheck(phones)
+    .then(notAppleOrGoogle)
+    .then(afford)
+    .then(options)
+    .catch((err) => console.log('Well that ain\'t right.', err))
+
 module.exports = {
-    // last2YearCheck, notAppleOrGoogle, afford, options
+    last2YearCheck, notAppleOrGoogle, afford, options
 }
--- index.test.js
+++ index.test.js
@@ -1,4 +1,4 @@
-const {options, last2YearCheck, notAppleOrGoogle, afford} = require('./index')
+const { options, last2YearCheck, notAppleOrGoogle, afford } = require('./index')
 
 describe(`Type Checking`, () => {
     it(`should return an Array`, () => {
@@ -10,11 +10,11 @@ describe(`Type Checking`, () => {
 })
 
 describe(`Manufactured items check from the last 2 years`, () => {
-    const years = [{year: 2020}, {year: 2020}, {year: 2019}, {year: 2018}, {year: 2022} , {year: 2010}]
-    const years2 = [{year: 2022}, {year: 2015}, {year: 2015}, {year: 2010}, {year: 2021}]
-    const expected = [{year: 2020}, {year: 2020}, {year: 2019}, {year: 2018}]
+    const years = [{ year: 2020 }, { year: 2020 }, { year: 2019 }, { year: 2018 }, { year: 2022 }, { year: 2010 }]
+    const years2 = [{ year: 2022 }, { year: 2015 }, { year: 2015 }, { year: 2010 }, { year: 2021 }]
+    const expected = [{ year: 2020 }, { year: 2020 }, { year: 2019 }, { year: 2018 }]
     const err = "nothing found from the last 2 years"
-    
+
     it('should only return phones from 2018-20', () => {
         expect(last2YearCheck(years)).toEqual(expected)
     })
@@ -24,22 +24,22 @@ describe(`Manufactured items check from the last 2 years`, () => {
 })
 
 describe(`Check if phones manufactured by Apple or Google`, () => {
-    const manufacturers = [{manufacturer: "Xiaomi"}, {manufacturer: "Samsung"}, {manufacturer: "OnePlus"}, {manufacturer: "Google"}, {manufacturer: "Apple"}, {manufacturer: "Motorola"} ]
-    const manufacturers2 = [{manufacturer: "Google"}, {manufacturer: "Apple"}]
-    const expected = [{manufacturer: "Xiaomi"}, {manufacturer: "Samsung"}, {manufacturer: "OnePlus"}, {manufacturer: "Motorola"} ]
+    const manufacturers = [{ manufacturer: "Xiaomi" }, { manufacturer: "Samsung" }, { manufacturer: "OnePlus" }, { manufacturer: "Google" }, { manufacturer: "Apple" }, { manufacturer: "Motorola" }]
+    const manufacturers2 = [{ manufacturer: "Google" }, { manufacturer: "Apple" }]
+    const expected = [{ manufacturer: "Xiaomi" }, { manufacturer: "Samsung" }, { manufacturer: "OnePlus" }, { manufacturer: "Motorola" }]
     const err = "No phones from other manufacturers found"
     it(`Google and Apple should not be included in the returned value`, () => {
         expect(notAppleOrGoogle(manufacturers)).toEqual(expected)
     })
-    it(`Should result in the function throwing if only Google or Apple: "No phones from other manufacturers found"`, () =>{
+    it(`Should result in the function throwing if only Google or Apple: "No phones from other manufacturers found"`, () => {
         expect(notAppleOrGoogle(manufacturers2)).toThrow(err)
     })
 })
 
 describe(`Available money check`, () => {
     const wallet = 500
-    const costs = [{cost: 898.00}, {cost: 269.99}, {cost: 279.00}, {cost: 467.33}, {cost: 625.00}, {cost: 344.04}, {cost: 150.00}]
-    const expected = [{cost: 269.99}, {cost: 279.00}, {cost: 467.33}, {cost: 344.04}, {cost: 150.00}]
+    const costs = [{ cost: 898.00 }, { cost: 269.99 }, { cost: 279.00 }, { cost: 467.33 }, { cost: 625.00 }, { cost: 344.04 }, { cost: 150.00 }]
+    const expected = [{ cost: 269.99 }, { cost: 279.00 }, { cost: 467.33 }, { cost: 344.04 }, { cost: 150.00 }]
     const err = "Not enough money to purchase something today"
 
     it(`items > 500.00 should be removed`, () => {
@@ -47,11 +47,11 @@ describe(`Available money check`, () => {
     })
 
     it(`The function should throw when everything too much: "Not enough money to purchase something today"`)
-        expect(afford(50.00, costs)).toThrow(err)
+    expect(afford(50.00, costs)).toThrow(err)
 })
 
 describe(`The final array of available phones`, () => {
-    const expected = [{"name": "Galaxy A50"}, {"name": "Galaxy M51"},{"name": "Pixel 3a"}]
+    const expected = [{ "name": "Galaxy A50" }, { "name": "Galaxy M51" }, { "name": "Pixel 3a" }]
 
     it(`should return an array of ojects with everything non valid filtered out`, () => {
         expect(options).toEqual(expected)
